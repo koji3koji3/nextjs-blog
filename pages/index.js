f8c5f1aebe0defb98fe752e8a5c4cb60eb35f6b4
shota kojima <kojima.shota@ffproject.onmicrosoft.com>
2020-07-21 15:22:30 +0900

    update : コメント修正

--- pages/index.js
+++ pages/index.js
@@ -13,13 +13,13 @@ export async function getStaticProps() {
   }
 }
 
-export async function getServerSideProps(context) {
-  return {
-    props: {
-      // props for your component
-    }
-  }
-}
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       // props for your component
+//     }
+//   }
+// }
 
 export default function Home({ allPostsData }) {
   return (
