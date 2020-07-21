888fa0e3b69dcc21adb4318d9cbf7846803ac759
shota kojima <kojima.shota@ffproject.onmicrosoft.com>
2020-07-21 21:52:18 +0900

    update : [id].tsxのエラーをけしました

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -46,8 +46,8 @@ export const getStaticPaths: GetStaticPaths = async () => {
 //     }
 //   }
 // }
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const postData = await getPostData(params.id as string)
+export const getStaticProps: GetStaticProps = async (params: any ) => {
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
